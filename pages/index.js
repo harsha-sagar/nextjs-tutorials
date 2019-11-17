325581f2531dd6b7dc0f767d8cd1db41207906f3
Harsha Sagar <mailtoharshasagar@gmail.com>
2019-11-17 23:30:19 +0530

    navigating between pages

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Link from 'next/link';
 
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a title="About Page">About Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 );
